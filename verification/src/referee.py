82a6909e0c702a38bb6a9ac9e459cb6ecf2f09a0
Valentin Bryukhanov <bvv.mag@gmail.com>
2015-03-25 10:40:35 +0300

    refactors

--- verification/src/referee.py
+++ verification/src/referee.py
@@ -8,9 +8,9 @@ from tests import TESTS
 
 class Referee(RefereeBase):
     TESTS = TESTS
-    EXECUTABLE_PATH = settings.EXECUTABLE_PATH
-    CURRENT_ENV = settings_env.CURRENT_ENV
-    FUNCTION_NAME = "morse_time"
+    ENVIRONMENTS = settings_env.ENVIRONMENTS
+
+    DEFAULT_FUNCTION_NAME = "morse_time"
     ENV_COVERCODE = {
         "python_2": covercodes.py_2_str,
         "python_3": None,
